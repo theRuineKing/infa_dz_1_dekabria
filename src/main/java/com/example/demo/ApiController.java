5306b1b5338477e1bb41a59d7a57d6338cabab46
Nikita <nikandri2008@gmail.com>
2025-12-02 00:07:54 +0300

    aboba commit ooo yeeaas

--- src/main/java/com/example/demo/ApiController.java
+++ src/main/java/com/example/demo/ApiController.java
@@ -11,6 +11,28 @@ import java.util.List;
 public class ApiController {
     private final List<String> messages = new ArrayList<>();
 
+    /* @GetMapping("getall")
+    public ResponseEntity<List<String>> getAll() {
+        return ResponseEntity.ok(messages);
+    }
+    @GetMapping("getById/{id}")
+    public ResponseEntity<String> getMessageById(@PathVariable int id) {
+        return ResponseEntity.ok(messages.get(id));
+    }
+    @GetMapping("deleteById/{id}")
+    public void deleteMessageById(@PathVariable int id) {
+        messages.remove(id);
+    }
+    @PostMapping("createMessage")
+    public void createMessage(@RequestBody String myText) {
+        messages.add(myText);
+    }
+    @PutMapping("update/{id}")
+    public void updateMessageById(@PathVariable int id, @RequestBody String text) {
+        messages.remove(id);
+        messages.add(id, text);
+    } */
+
     // curl --location 'localhost:8081/messages'
     @GetMapping("messages")
     public ResponseEntity<List<String>> getMessages() {
@@ -51,5 +73,45 @@ public class ApiController {
         messages.remove((int) i);
         messages.add(i, message);
         return ResponseEntity.accepted().build();
+    }
+
+    //curl --location 'localhost:8081/messages/search/EASPORTS'
+    @GetMapping("/messages/search/{text}")
+    public ResponseEntity<Integer> getTextIndex(@PathVariable String text) {
+        int foundedIndex = 0;
+        for (String s : messages) {
+            if (s.equals(text)) {
+                return ResponseEntity.ok(foundedIndex);
+            }
+            foundedIndex++;
+        }
+        return ResponseEntity.ok(-1);
+    }
+
+    //curl --location 'localhost:8081/messages/count'
+    @GetMapping("/messages/count")
+    public ResponseEntity<Integer> howManyMessages() {
+        return ResponseEntity.ok(messages.size());
+    }
+
+    //curl --location 'localhost:8081/messages/1/create' \
+    //--header 'Content-Type: application/json' \
+    //--data 'EASPORTS'
+    @PostMapping("/messages/{index}/create")
+    public ResponseEntity<Void> createMessage(@PathVariable int index, @RequestBody String text) {
+        messages.add(index, text);
+        return ResponseEntity.noContent().build();
+    }
+
+    //curl --location --request DELETE 'localhost:8081/messages/search/EASPORTS'
+    @DeleteMapping("/messages/search/{text}")
+    public ResponseEntity<Void> deleteSubstring(@PathVariable String text) {
+        int i = 0;
+        for (String s : messages) {
+            if (s.contains(text)) {
+                messages.remove(i);
+            }
+        }
+        return ResponseEntity.noContent().build();
     }
 }
